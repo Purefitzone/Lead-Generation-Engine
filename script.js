e7c80adadfa88ed7ad7b2f6868439c4c9932e10c
Purefitzone <kiprotich680@gmail.com>
2026-03-08 20:39:07 +0300

    Update webhook URL in form submission

--- script.js
+++ script.js
@@ -1,31 +1,22 @@
 document.getElementById("leadForm").addEventListener("submit", function(e){
-
 e.preventDefault();
-
 const name = document.querySelector("input[type='text']").value;
 const email = document.querySelector("input[type='email']").value;
-
-fetch("https://hook.make.com/pwopi0r8ufitjcm3qoaybutp1alj6ud5", {
-
+fetch("https://hook.us2.make.com/pwopi0r8ufitjcm3qoaybutp1alj6ud5", {
 method: "POST",
-
 headers: {
 "Content-Type": "application/json"
 },
-
 body: JSON.stringify({
 name: name,
 email: email,
 source: "Landing Page",
 date: new Date().toISOString()
 })
-
 })
-
 .then(() => {
 document.getElementById("successMessage").innerText =
 "Thank you! Your information has been received.";
 });
-
 });
 
